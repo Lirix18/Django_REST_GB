0e6641380f2f58c709e8498e804115e703a94801
AnnaKalashnikova <kushina1809@gmail.com>
2023-02-22 17:05:15 +0300

    Задание 12 docker nginx for frontend

--- TODO/backend/settings.py
+++ TODO/backend/settings.py
@@ -22,9 +22,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-+dcd4n^%y9gug^2#5(xf9k0(&_+=6f-5x_#wx_fvu)9*sf@v1s'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
