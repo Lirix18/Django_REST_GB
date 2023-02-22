7bb0f852a77c634bcaa5f8c3810a308300116047
AnnaKalashnikova <kushina1809@gmail.com>
2023-02-22 15:40:21 +0300

    Задание 12 docker django

--- TODO/backend/settings.py
+++ TODO/backend/settings.py
@@ -86,10 +86,16 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'library',
+        'USER': 'dante',
+        'PASSWORD': 'dante123456',
+        'HOST': 'db',
+        'POST': '5432',
     }
 }
+
+# FIXTURE_DIRS = 'TODO/fixtures/'
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
